c0309b65e3b8a6e819e23e6fc6e6c184e66799c2
dubidubidubaPerry <damarino@uninorte.edu.co>
2021-12-11 12:28:28 -0500

    Updated

--- corpabanca/src/components/paginas/Inicio.js
+++ corpabanca/src/components/paginas/Inicio.js
@@ -3,29 +3,32 @@ import React from 'react'
 const Inicio = () => {
     return (
         <div>
-            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                    <img src="..." className="d-block w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                    <img src="..." className="d-block w-100" alt="..."/>
-                    </div>
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-100" src="./CorpBanca-en-Cali.jpg" alt="First slide"/>
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Anterior</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Siguiente</span>
-                </button>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="./CorpBanca-en-Cali.jpg" alt="Second slide"/>
                 </div>
-
-
+                <div class="carousel-item">
+                <img class="d-block w-100" src="./CorpBanca-en-Cali.jpg" alt="Third slide"/>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            </div>
         </div>
     )
 }
